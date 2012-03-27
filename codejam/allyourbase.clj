96b818d997d40a4ac018e8afd122285ca19dbdef
Anselm Strauss <amsibamsi@gmail.com>
2012-03-27 20:46:32 +0200

    fixed not starting with zero, added input file

--- codejam/allyourbase.clj
+++ codejam/allyourbase.clj
@@ -1,22 +1,33 @@
 ; google code jam 2009 round 1c
 
 (use 'clojure.contrib.generic.math-functions)
+(use 'clojure.java.io)
 
-(def message (seq (first *command-line-args*)))
+; first character is not a zero
+(defn significance [sym alphabet]
+  (let [index (.indexOf alphabet sym)]
+    (if (== index 0)
+      1
+      (if (== index 1)
+        0
+        index))))
 
-(def alphabet (distinct message))
-
-(def base (count alphabet))
-
-(defn minimum [input sum]
+(defn find-minimum [input alphabet base sum]
   (if (empty? input)
     sum
     (let
       [sym (first input)
-       digit (.indexOf alphabet sym)
-       position (count input)
+       digit (significance sym alphabet)
+       position (- (count input) 1)
        value (* digit (pow base position))]
-      (println sum)
-      (minimum (rest input) (+ sum value)))))
+      (find-minimum (rest input) alphabet base (+ sum value)))))
 
-(println (minimum message 0))
+(defn minimum [message]
+  (let [symbols (seq message)
+        alphabet (distinct symbols)
+        base (count alphabet)]
+    (find-minimum symbols alphabet base 0)))
+
+(with-open [rdr (reader "A-small-practice.in")]
+  (doseq [message (line-seq rdr)]
+    (println (minimum message))))

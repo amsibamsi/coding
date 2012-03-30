6ab8baed23d1f6b3ff30d33190d49ab761836da9
Anselm Strauss <amsibamsi@gmail.com>
2012-03-31 00:02:06 +0200

    proper output

--- codejam/allyourbase.clj
+++ codejam/allyourbase.clj
@@ -1,7 +1,7 @@
 ; google code jam 2009 round 1c
 
-(use 'clojure.contrib.generic.math-functions)
 (use 'clojure.java.io)
+(use 'clojure.string)
 
 ; first character is not a zero
 (defn significance [sym alphabet]
@@ -14,12 +14,12 @@
 
 (defn find-minimum [input alphabet base sum]
   (if (empty? input)
-    sum
+    (int sum)
     (let
       [sym (first input)
        digit (significance sym alphabet)
        position (- (count input) 1)
-       value (* digit (pow base position))]
+       value (* digit (Math/pow base position))]
       (find-minimum (rest input) alphabet base (+ sum value)))))
 
 (defn minimum [message]
@@ -28,6 +28,13 @@
         base (count alphabet)]
     (find-minimum symbols alphabet base 0)))
 
-(with-open [rdr (reader "A-small-practice.in")]
-  (doseq [message (line-seq rdr)]
-    (println (minimum message))))
+(defn output [messages counter]
+  (if (empty? messages)
+    nil
+    (do
+      (print (str "Case #" counter ": "))
+      (println (minimum (first messages)))
+      (output (rest messages) (+ counter 1)))))
+
+(def msg (rest (split-lines (slurp "A-small-practice.in"))))
+(output msg 1)

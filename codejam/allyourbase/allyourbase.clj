4170eb4e0a2b0a900bdc87bad158b83bbe14940e
Anselm Strauss <amsibamsi@gmail.com>
2012-04-05 10:07:09 +0200

    fix: using new version, old did compute wrong alphabet

--- codejam/allyourbase/allyourbase.clj
+++ codejam/allyourbase/allyourbase.clj
@@ -7,48 +7,38 @@
 ; has value 0, second new symbol is 1, third new is 2, etc.
 ; exception: number has no leading zeroes, so first symbol is 1 and second is 0
 ; example: a8a = 101 in binary = 5 in decimal
-(defn minimum-value [digit alphabet]
-  (let [value (.indexOf alphabet digit)]
-    (if (== value 0)
-      1
-      (if (== value 1)
-        0
-        value))))
+(defn minimum-alphabet [number alphabet]
+  (if (empty? number)
+    alphabet 
+    (let
+      [symbol (first number)
+       size (count alphabet)
+       added (if (nil? (alphabet symbol))
+             (case size
+               0 {symbol 1}
+               1 {symbol 0}
+               {symbol size})
+             {})]
+      (minimum-alphabet (rest number) (merge alphabet added)))))
 
-(defn minimum-decimal [number alphabet base sum]
+(defn minimum [number alphabet sum]
   (if (empty? number)
     (long sum)
     (let
       [digit (first number)
-       value (minimum-value digit alphabet)
-       magnitude (- (count number) 1)
-       decimal (* value (Math/pow base magnitude))]
-      (minimum-decimal (rest number) alphabet base (+ sum decimal)))))
+       value (alphabet digit)
+       ; exception: base is not unary
+       base (max 2 (count alphabet))]
+      (minimum (rest number) alphabet (+ (* sum base) value)))))
 
-(defn minimum [message]
-  (let
-    [symbols (seq message)
-     alphabet (distinct symbols)
-     ; exception: base 1 not allowed
-     base (max 2 (count alphabet))]
-    (str (minimum-decimal symbols alphabet base 0))))
+(defn parse [messages current total]
+  (if (<= current total)
+    (let
+      [number (seq (first messages))
+       minimum (minimum number (minimum-alphabet number {}) 0)]
+      (println (str "Case #" current ": " minimum))
+      (parse (rest messages) (+ current 1) total))
+    nil))
 
-(defn parse [messages counter amount output]
-  (if (<= counter amount)
-    (parse
-      (rest messages)
-      (+ counter 1)
-      amount
-      (conj output (str "Case #" counter ": " (minimum (first messages)))))
-    output))
-
-(defn parse-file [in out]
-  (let
-    [input (split-lines (slurp in))
-     amount (read-string (first input))
-     messages (rest input)
-     result (parse messages 1 amount [])]
-    (spit out (str (join "\n" result) "\n"))))
-
-(parse-file "small.in" "small.out")
-(parse-file "large.in" "large.out")
+(def input (line-seq (java.io.BufferedReader. *in*)))
+(parse (rest input) 1 (read-string (first input)))

3f7aea7878ff97d58f320b0a1eecd39685ec1227
Anselm Strauss <amsibamsi@gmail.com>
2012-10-01 00:43:53 +0200

    extracted lets into own defs

--- examples/jmetest/src/jmetest/core.clj
+++ examples/jmetest/src/jmetest/core.clj
@@ -1,5 +1,6 @@
 (ns jmetest.core
   (:import (com.jme3.app SimpleApplication)
+           (com.jme3.system JmeSystem)
            (com.jme3.material Material)
            (com.jme3.math Vector3f)
            (com.jme3.scene Geometry)
@@ -9,17 +10,18 @@
   (:gen-class
     :extends com.jme3.app.SimpleApplication))
 
+(def asset-url (.getResource (.getContextClassLoader (Thread/currentThread)) "com/jme3/asset/Desktop.cfg"))
+(def asset-manager (JmeSystem/newAssetManager asset-url))
 (def box (new Box Vector3f/ZERO 1 1 1))
 (def geometry (new com.jme3.scene.Geometry "Box" box))
+(def material (new Material asset-manager "Common/MatDefs/Misc/Unshaded.j3md"))
+(def sky (SkyFactory/createSky asset-manager "Textures/Sky/Bright/BrightSky.dds" false))
 
 (defn -simpleInitApp [this]
-  (let
-    [material (new Material (.getAssetManager this) "Common/MatDefs/Misc/Unshaded.j3md")
-     sky (SkyFactory/createSky (.getAssetManager this) "Textures/Sky/Bright/BrightSky.dds" false)]
-    (.setColor material "m_Color" ColorRGBA/Blue)
-    (.setMaterial geometry material)
-    (.attachChild (.getRootNode this) geometry)
-    (.attachChild (.getRootNode this) sky)))
+  (.setColor material "m_Color" ColorRGBA/Blue)
+  (.setMaterial geometry material)
+  (.attachChild (.getRootNode this) geometry)
+  (.attachChild (.getRootNode this) sky))
 
 (defn -main [& args]
   (doto (new jmetest.core) (.start)))

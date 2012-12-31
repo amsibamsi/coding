8e2b9f48fe64bbd3361ec0bbc3a6cd5cc5612410
Anselm Strauss <amsibamsi@gmail.com>
2012-12-31 11:36:35 +0100

    added Env, Hello; configure ant classpath for running

---
+++ java-exercises/src/Env.java
@@ -0,0 +1,18 @@
+import java.net.URL;
+import java.net.URLClassLoader;
+ 
+public class Env {
+ 
+  public static void main (String args[]) {
+ 
+    ClassLoader cl = ClassLoader.getSystemClassLoader();
+ 
+    URL[] urls = ((URLClassLoader)cl).getURLs();
+ 
+    for(URL url: urls){
+      System.out.println(url.getFile());
+    }
+ 
+  }
+
+}

d50b8ce44ce1c433fa5d6e156ecf15f04e0f3e7c
Anselm Strauss <amsibamsi@gmail.com>
2013-04-14 13:30:09 +0200

    rm due to rename

--- codejam/2013/src/gcj2013/a.clj
+++
@@ -1,4 +0,0 @@
-(ns gcj2013.a)
-
-(defn sample []
-  (slurp "res/a-sample.out"))
--- codejam/2013/test/gcj2013/a_test.clj
+++
@@ -1,7 +0,0 @@
-(ns gcj2013.a-test
-  (:use clojure.test)
-  (:use gcj2013.a))
-
-(deftest test-sample
-  (testing "sample output")
-  (is (= (sample) (slurp "res/a-sample.out"))))

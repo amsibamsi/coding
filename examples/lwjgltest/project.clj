e2c5ee2cc4ae9ffe779decc525b8c0f605d06299
Anselm Strauss <amsibamsi@gmail.com>
2013-04-26 08:42:53 +0200

    added natives for all 3 platforms

--- examples/lwjgltest/project.clj
+++ examples/lwjgltest/project.clj
@@ -1,7 +1,9 @@
 (defproject lwjgltest "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.lwjgl.lwjgl/lwjgl "2.9.0"]
-                 [org.lwjgl.lwjgl/lwjgl-platform "2.9.0" :classifier "natives-osx" :native-prefix ""]]
+                 [org.lwjgl.lwjgl/lwjgl-platform "2.9.0" :classifier "natives-osx" :native-prefix ""]
+                 [org.lwjgl.lwjgl/lwjgl-platform "2.9.0" :classifier "natives-linux" :native-prefix ""]
+                 [org.lwjgl.lwjgl/lwjgl-platform "2.9.0" :classifier "natives-windows" :native-prefix ""]]
   :min-lein-version "2.1.3"
   :javac-target "1.6"
   :main lwjgltest.start)

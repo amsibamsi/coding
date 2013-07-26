07fd29cd99871bbaed2912aa7e501b7c9d4936dd
Anselm Strauss <amsibamsi@gmail.com>
2013-07-26 14:24:29 +0200

    simulating service push

--- examples/pedestal/client/app/src/client/behavior.clj
+++ examples/pedestal/client/app/src/client/behavior.clj
@@ -1,7 +1,7 @@
 (ns ^:shared client.behavior
-    (:require [clojure.string :as string]
-              [io.pedestal.app.messages :as msg]
-              [io.pedestal.app :as app]))
+  (:require [clojure.string :as string]
+            [io.pedestal.app.messages :as msg]
+            [io.pedestal.app :as app]))
 
 ;(defn set-value-transform [old-value message]
 ;  (:value message))
@@ -9,12 +9,16 @@
 (defn inc-transform [old-value _]
   ((fnil inc 0) old-value))
 
+(defn swap-transform [_ message]
+  (:value message))
+
 (defn init-main [_]
   [[:transform-enable [:main :my-counter] :inc [{msg/topic [:my-counter]}]]])
 
 (def example-app
   {:version 2
-   :transform [[:inc [:my-counter] inc-transform]]
+   :transform [[:inc [:my-counter] inc-transform]
+               [:swap [:**] swap-transform]]
    :emit [{:init init-main}
-          [#{[:*]} (app/default-emitter [:main])]]})
+          [#{[:my-counter] [:other-counters :*]} (app/default-emitter [:main])]]})
 
